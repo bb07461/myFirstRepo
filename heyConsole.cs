a91b69b8b791cfa2389486d52a201a5e97c2bac7
bb07461 <98509031+bb07461@users.noreply.github.com>
2024-09-26 21:21:25 -0400

    Update heyConsole.cs
    hello world mess

--- heyConsole.cs
+++ heyConsole.cs
@@ -1,1 +1,11 @@
 
+using System; 
+
+namespace HelloWorldApp{
+    class Ugo{
+        static void Main(string[] args){
+            Console.WriteLine("Hello World! this is my first repo from vs code")
+            Console.ReadKey();
+        }
+    }
+}
